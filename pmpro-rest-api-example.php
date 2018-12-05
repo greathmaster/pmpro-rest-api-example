af342514405713ed81960b01a2bc4bd2887819b1
greathmaster <counthmaster@yahoo.com>
2018-12-05 11:37:03 -0800

    General code cleanup

--- pmpro-rest-api-example.php
+++ pmpro-rest-api-example.php
@@ -8,32 +8,31 @@ Author: Hersha Venkatesh
 Author URI: 
 */
 
+
 function pmpro_rest_api_client_example()
 {
 	/*
 	Basic Authentication string ie: base64_encode($username . ':' . $password)
 	$username and $password are credentials to remote server running REST API
 	Only use Basic Auth for testing/dev environments in production use OAuth 1 or 2.
-    	*/
-	
-	$auth = ""; 
+   	*/
+    
+    $username = "";
+    $password = "";
+	$auth = base64_encode($username . ':' . $password);
 		
-	$remote_url = ""; //Ex: "http://example.com/wp-json/wp/v2/users/2/pmpro_membership_level";
-	
+	//$remote_url = "https://example.com/wp-json/wp/v2/posts/58/user_id/2/pmpro_has_membership_access";
+	$remote_url = "https://example.com/wp-json/wp/v2/users/1404/pmpro_membership_level"; 
 	$args = array(
     			'headers'=>
-    				array('authorization' =>'Basic '.$auth),
+    				array('Authorization' =>'Basic '.$auth),
 	);
 	
 	$response = wp_remote_get($remote_url, $args);
 	$data =  wp_remote_retrieve_body($response);
-	
-	echo "<pre>";
-	var_dump(json_decode( $data ));
-	echo "</pre>";
+
+	return print_r(json_decode($data), true);
+		
 }
 
 add_shortcode('pmpro_rest_api_example', 'pmpro_rest_api_client_example');
-
-
-
